af0cdf53869c99439bb18bff118da9b703b6a6c5
MiepHD <63968466+MiepHD@users.noreply.github.com>
2025-04-21 19:25:04 +0200

    Hours and minutes face can be stopped and resumed

--- scripts/face/FaceRotator.js
+++ scripts/face/FaceRotator.js
@@ -1,15 +1,23 @@
 class FaceRotator {
   id;
   speed;
+  rotating;
   constructor(id) {
     this.id = id;
     this.speed = Math.random() * 60 - 30;
+    this.rotating = true;
     document.getElementById('speed-slider-' + this.id).value = this.speed;
     document
       .getElementById('speed-slider-' + this.id)
       .addEventListener('input', this.setSpeed.bind(this));
+    document
+      .getElementById(this.id + '-moving')
+      .addEventListener('input', (e) => {
+        this.rotating = e.target.checked;
+      });
   }
   updateRotation() {
+    if (!this.rotating) return;
     document
       .getElementById('digits-' + this.id)
       .style.setProperty(
